524ef0d719bddb08e0932bdbe94e43ab90d1e233
Sanoj Punchihewa <sanoj.punchihewa@gmail.com>
2021-01-22 14:52:05 +0530

    Add integration tests for mi get apis command

---
+++ import-export-cli/mi/integration/api_test.go
@@ -0,0 +1,93 @@
+/*
+*  Copyright (c) WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+ */
+
+package integration
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/wso2/product-apim-tooling/import-export-cli/integration/base"
+	"github.com/wso2/product-apim-tooling/import-export-cli/mi/utils/artifactutils"
+)
+
+const validAPIName = "HealthcareAPI"
+const invalidAPIName = "abcAPI"
+const apisCmd = "apis"
+
+func TestGetAPIs(t *testing.T) {
+	ValidateAPIsList(t, config)
+}
+
+func TestGetAPIByName(t *testing.T) {
+	ValidateAPI(t, config, validAPIName)
+}
+
+func TestGetNonExistingAPIByName(t *testing.T) {
+	response, _ := getArtifact(t, apisCmd, invalidAPIName, config)
+	base.Log(response)
+	assert.Contains(t, response, "[ERROR]: Getting Information of "+apisCmd+" [ "+invalidAPIName+" ]  404 Not Found")
+}
+
+func TestGetAPIsWithoutSettingUpEnv(t *testing.T) {
+	getCommandWithoutSettingEnv(t, apisCmd)
+}
+
+func TestGetAPIsWithoutLogin(t *testing.T) {
+	getCommandWithoutLogin(t, apisCmd, config)
+}
+
+func TestGetAPIsWithoutEnvFlag(t *testing.T) {
+	getCommandWithoutEnvFlag(t, apisCmd, config)
+}
+
+func TestGetAPIsWithInvalidArgs(t *testing.T) {
+	getCommandWithInvalidArgs(t, config, 1, 2, apisCmd, validAPIName, invalidAPIName)
+}
+
+func ValidateAPIsList(t *testing.T, config *MiConfig) {
+	t.Helper()
+	output, _ := listArtifacts(t, apisCmd, config)
+	artifactList := config.MIClient.GetArtifactList(&artifactutils.IntegrationAPIList{})
+	ValidateAPIListEqual(t, output, (artifactList.(*artifactutils.IntegrationAPIList)))
+}
+
+func ValidateAPIListEqual(t *testing.T, apisListFromCtl string, apisList *artifactutils.IntegrationAPIList) {
+	unmatchedCount := apisList.Count
+	for _, api := range apisList.Apis {
+		assert.Truef(t, strings.Contains(apisListFromCtl, api.Name), "apisListFromCtl: "+apisListFromCtl+
+			" , does not contain api.Name: "+api.Name)
+		unmatchedCount--
+	}
+	assert.Equal(t, 0, int(unmatchedCount), "API lists are not equal")
+}
+
+func ValidateAPI(t *testing.T, config *MiConfig, apiName string) {
+	t.Helper()
+	output, _ := getArtifact(t, apisCmd, apiName, config)
+	artifactList := config.MIClient.GetArtifact("apiName", apiName, &artifactutils.IntegrationAPI{})
+	ValidateAPIEqual(t, output, (artifactList.(*artifactutils.IntegrationAPI)))
+}
+
+func ValidateAPIEqual(t *testing.T, apisListFromCtl string, api *artifactutils.IntegrationAPI) {
+	assert.Contains(t, apisListFromCtl, api.Name)
+	assert.Contains(t, apisListFromCtl, api.Stats)
+	assert.Contains(t, apisListFromCtl, api.Url)
+	assert.Contains(t, apisListFromCtl, api.Version)
+}
---
+++ import-export-cli/mi/integration/getArtifacts_testUtils.go
@@ -0,0 +1,119 @@
+/*
+*  Copyright (c) WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+ */
+
+package integration
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/wso2/product-apim-tooling/import-export-cli/integration/base"
+	"github.com/wso2/product-apim-tooling/import-export-cli/utils"
+)
+
+func listArtifacts(t *testing.T, artifactType string, config *MiConfig) (string, error) {
+	t.Helper()
+	base.SetupMIEnv(t, config.MIClient.GetEnvName(), config.MIClient.GetMiURL())
+	base.MILogin(t, config.MIClient.GetEnvName(), config.Username, config.Username)
+	output, err := base.Execute(t, "mi", "get", artifactType, "-e", config.MIClient.GetEnvName())
+	return output, err
+}
+
+func getArtifact(t *testing.T, artifactType, artifactName string, config *MiConfig) (string, error) {
+	t.Helper()
+	base.SetupMIEnv(t, config.MIClient.GetEnvName(), config.MIClient.GetMiURL())
+	base.MILogin(t, config.MIClient.GetEnvName(), config.Username, config.Username)
+	output, err := base.Execute(t, "mi", "get", artifactType, artifactName, "-e", config.MIClient.GetEnvName())
+	return output, err
+}
+
+// GetArtifactList : Get Artifact Lists from MI Management API
+func (instance *MiRESTClient) GetArtifactList(artifactListType interface{}) interface{} {
+	apisURL := getResourceURL(instance.GetMiURL(), utils.MiManagementAPIResource)
+
+	request := base.CreateGet(apisURL)
+	base.SetDefaultRestAPIHeaders(instance.accessToken, request)
+	base.LogRequest("mi.GetArtifactList()", request)
+	response := base.SendHTTPRequest(request)
+	defer response.Body.Close()
+
+	base.ValidateAndLogResponse("mi.GetArtifactList()", response, 200)
+
+	artifactListResponse := artifactListType
+	json.NewDecoder(response.Body).Decode(&artifactListResponse)
+	return artifactListResponse
+}
+
+// GetArtifact : Get Artifacts from MI Management API
+func (instance *MiRESTClient) GetArtifact(artifactKey, artifactName string, artifactType interface{}) interface{} {
+	apisURL := getResourceURLWithQueryParam(instance.GetMiURL(), utils.MiManagementAPIResource, artifactKey, artifactName)
+
+	request := base.CreateGet(apisURL)
+	base.SetDefaultRestAPIHeaders(instance.accessToken, request)
+	base.LogRequest("mi.GetArtifact()", request)
+	response := base.SendHTTPRequest(request)
+	defer response.Body.Close()
+
+	base.ValidateAndLogResponse("mi.GetArtifact()", response, 200)
+
+	artifactListResponse := artifactType
+	json.NewDecoder(response.Body).Decode(&artifactListResponse)
+	return artifactListResponse
+}
+
+func getCommandWithoutSettingEnv(t *testing.T, artifactType string) {
+	t.Helper()
+	response, _ := base.Execute(t, "mi", "get", artifactType, "-e", "testing")
+	base.GetRowsFromTableResponse(response)
+	base.Log(response)
+	assert.Contains(t, response, "MI does not exists in testing Add it using add env")
+}
+
+func getCommandWithoutLogin(t *testing.T, artifactType string, config *MiConfig) {
+	t.Helper()
+	base.SetupMIEnv(t, config.MIClient.GetEnvName(), config.MIClient.GetMiURL())
+	response, _ := base.Execute(t, "mi", "get", artifactType, "-e", "testing")
+	base.GetRowsFromTableResponse(response)
+	base.Log(response)
+	assert.Contains(t, response, "Login to MI")
+}
+
+func getCommandWithoutEnvFlag(t *testing.T, artifactType string, config *MiConfig) {
+	t.Helper()
+	base.SetupMIEnv(t, config.MIClient.GetEnvName(), config.MIClient.GetMiURL())
+	base.MILogin(t, "testing", adminUserName, adminPassword)
+	response, _ := base.Execute(t, "mi", "get", artifactType)
+	base.GetRowsFromTableResponse(response)
+	base.Log(response)
+	assert.Contains(t, response, `required flag(s) "environment" not set`)
+}
+
+func getCommandWithInvalidArgs(t *testing.T, config *MiConfig, required, passed int, args ...string) {
+	t.Helper()
+	base.SetupMIEnv(t, config.MIClient.GetEnvName(), config.MIClient.GetMiURL())
+	base.MILogin(t, "testing", adminUserName, adminPassword)
+	getCmdArgs := []string{"mi", "get"}
+	getCmdArgs = append(getCmdArgs, args...)
+	response, _ := base.Execute(t, getCmdArgs...)
+	base.GetRowsFromTableResponse(response)
+	base.Log(response)
+	expected := fmt.Sprintf("accepts at most %v arg(s), received %v", required, passed)
+	assert.Contains(t, response, expected)
+}
